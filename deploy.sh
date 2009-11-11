9161327c3b4e56cfe474f2053687addacb440b87
Michael Markert <markert.michael@gmail.com>
2009-11-11 22:13:20 +0100

    Fixed deploy script.

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@ for file in `ls`; do
     case $file in
         "vimenv_setup")
             echo "foo" $file
-            ln --symbolic --force -T `pwd`/$file /tmp/.$file
+            ln --symbolic --force -T `pwd`/$file ~/.$file
             ;;
         *)
             echo "bar" $file
-            ln --symbolic --force -T `pwd`/$file /tmp/.vimrc
+            ln --symbolic --force -T `pwd`/$file ~/.vimrc
             ;;
         esac
 done

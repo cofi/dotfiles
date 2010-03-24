0fded545057f15698b859c0b17ec74984cb045f5
Michael Markert <markert.michael@gmail.com>
2010-03-24 03:08:05 +0100

    [Emacs] Disabled load average in time mode.

--- emacs.d/cofi-modes.el
+++ emacs.d/cofi-modes.el
@@ -8,10 +8,15 @@
         )
       )
 
+(setq display-time-24hr-format t)
+;; dirty hack to suppress threshold showing
+(setq display-time-load-average-threshold 100)
+
 (mapc (lambda (mode)
         (funcall mode t))
       '(
         display-battery-mode
+        display-time-mode
         global-font-lock-mode
         global-hl-line-mode
         global-linum-mode
@@ -21,7 +26,5 @@
         ))
 
 (fringe-mode 'left-only)
-(setq display-time-24hr-format t)
-(display-time)
 
 (provide 'cofi-modes)

81cd0a7568e1d45732f5779e81523d7f4744f16f
Michael Markert <markert.michael@gmail.com>
2010-03-31 21:25:36 +0200

    [Emacs] Added notification funs.

--- emacs.d/cofi-util.el
+++ emacs.d/cofi-util.el
@@ -11,4 +11,25 @@
    (car pair)
    (cadr pair)))
 
+(defun plasma-send-notification (msg title &optional timeout)
+  "Send plasma notification."
+  (let* ((timeouts (if timeout (format "%d" timeout)
+                     "10"))
+         (command (format "kdialog --passivepopup '%s' --title '%s' %s" msg title timeouts)))
+    (shell-command-to-string command)))
+
+(defun libnotify-send-notification (msg title &optional timeout urgency)
+  "Send libnotify notification."
+  (let* ((timeouts (if timeout (format "%d" timeout)
+                     "10"))
+         (urgencys (if urgency (format "%s" urgency)
+                     "normal"))
+        (command
+             (format "notify-send -u %s -t %s '%s' '%s'" urgencys timeouts title msg)))
+    (shell-command-to-string command)))
+
+(defun send-notification (msg title &optional timeout)
+  "Sends notification."
+  (funcall 'plasma-send-notification msg title timeout))
+
 (provide 'cofi-util)

b0677d5deda285998e997de5c55254b8e9d5333d
Michael Markert <markert.michael@gmail.com>
2010-04-25 02:16:11 +0200

    [Emacs] Added eval-buffer binding for elisp modes.

--- emacs.d/cofi-lisp.el
+++ emacs.d/cofi-lisp.el
@@ -1,4 +1,9 @@
 (add-hook 'lisp-mode-hook 
           (lambda () (local-set-key (kbd "RET") 'newline-and-indent))) 
+
 (add-hook 'emacs-lisp-mode-hook
-          (lambda () (local-set-key (kbd "RET") 'newline-and-indent))) 
+          (lambda ()
+            (local-set-key (kbd "RET") 'newline-and-indent)
+            (local-set-key (kbd "C-c C-c") 'eval-buffer)))
+
+(provide 'cofi-lisp)

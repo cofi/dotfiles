94297124a989f0b00bc8a627f3a6f19a1c74475e
Michael Markert <markert.michael@gmail.com>
2010-04-29 12:41:48 +0200

    [Emacs] Fixed latex/tex hooks.

--- emacs.d/cofi-tex.el
+++ emacs.d/cofi-tex.el
@@ -1,5 +1,16 @@
-(add-hook 'TeX-mode-hook 'auto-fill-mode)
-(add-hook 'TeX-mode-hook 'flyspell-mode)
+(defun cofi-tex-latex-hook ()
+  ;; deactivate auto-complete, flyspell seems to mess with it when first active
+  (auto-complete-mode -1)
+
+  (auto-fill-mode 1)
+  (flyspell-mode 1)
+
+  ;; reactivate modes
+  (auto-complete-mode 1)
+  )
+
+(add-hook 'tex-mode-hook 'cofi-tex-latex-hook)
+(add-hook 'latex-mode-hook 'cofi-tex-latex-hook)
 
 (setq TeX-auto-save t
       TeX-parse-self t)

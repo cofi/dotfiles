506c2c55132ebfba2ea4a0525341689e7d5715df
Michael Markert <markert.michael@gmail.com>
2010-04-30 19:08:54 +0200

    [Emacs] Reduced vc-backends.

--- emacs.d/cofi-vc.el
+++ emacs.d/cofi-vc.el
@@ -1,8 +1,9 @@
+(setq vc-handled-backends '(SVN Hg))
+
 (add-hook 'vc-dir-mode-hook
           (lambda ()
-            (progn
               (local-set-key (kbd "j") 'vc-dir-next-line)
               (local-set-key (kbd "k") 'vc-dir-previous-line)
-              )))
+              ))
 
 (provide 'cofi-vc)

ee67ae79e9d72e638cda22f4ccafec7fa9fdcdba
Michael Markert <markert.michael@gmail.com>
2010-05-08 00:52:08 +0200

    [Emacs] Made okular sole viewer for auctex.

--- emacs.d/cofi-tex.el
+++ emacs.d/cofi-tex.el
@@ -7,6 +7,11 @@
 (add-hook 'TeX-language-de-hook (lambda ()
                                   (ispell-change-dictionary "de_DE-neu")))
 
+(setq TeX-output-view-style '(
+                              ("^pdf$" "." "okular %o")
+                              ("^dvi$" "." "okular %o")
+                              )
+
 (setq TeX-auto-save t
       TeX-parse-self t)
 

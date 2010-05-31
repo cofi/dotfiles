dd03d7385cf445e07be997f6a15d8269e1ee7db4
Michael Markert <markert.michael@gmail.com>
2010-05-31 14:12:34 +0200

    [Emacs] Added mail entry to automodes and let mail config be called interactively.

--- emacs.d/cofi-mail.el
+++ emacs.d/cofi-mail.el
@@ -1,14 +1,15 @@
 (defun cofi-do-mail ()
+  (interactive)
   (set-buffer-modified-p nil)
   (setq make-backup-files nil)
   (mail-mode)
   (turn-on-auto-fill)
   (setq fill-column 72)
   (ac-flyspell-mode)
-  (mail-text)
-  )
+  (mail-text))
 
 ;; serve sup
 (push '("\.compose-mode$" . cofi-do-mail) auto-mode-alist)
+(push '("\.reply-mode$" . cofi-do-mail) auto-mode-alist)
 
 (provide 'cofi-mail)

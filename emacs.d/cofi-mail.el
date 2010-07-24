fc3031089c46d356569c637f87ed741dfe483755
Michael Markert <markert.michael@gmail.com>
2010-07-24 21:23:18 +0200

    [Emacs] Added mail functions.

--- emacs.d/cofi-mail.el
+++ emacs.d/cofi-mail.el
@@ -13,4 +13,32 @@
 (autoload 'wl-other-frame "wl" "Wanderlust on new frame." t)
 (autoload 'wl-draft "wl-draft" "Write draft with Wanderlust." t)
 
+(defun cofi-wl-frame ()
+  "Start wl and set frame title."
+  (set-frame-size (selected-frame) 150 100)
+  (setq frame-title-format "Wanderlust Mail")
+  (wl))
+
+(defun mail-attachment-check ()
+  "Check if attachment is mentioned but not included"
+  (interactive)
+  (let ((attachment-regexp "[Aa]ttachment\\|[Aa]ttached\\|[Aa]nbei\\|[Bb]eiliegend\\|[Aa]anhang\\|angehängt"))
+    (save-excursion
+      (goto-char 0)
+      (unless (re-search-forward "^Content-Disposition: attachment" nil t)
+        (when
+            ;; Check for mentioned attachments
+            (re-search-forward attachment-regexp nil t)
+          (unless (y-or-n-p "Attachment maybe missing. Send? ")
+            (error "Abort.")))))))
+
+(defun mail-subject-check ()
+  "Check if subject is missing."
+  (unless (or (> (length (std11-field-body "Subject")) 1)
+          (y-or-n-p "Subject missing. Send? "))
+      (error "Abort.")))
+
+(add-hook 'wl-mail-send-pre-hook 'mail-attachment-check)
+(add-hook 'wl-mail-send-pre-hook 'mail-subject-check)
+
 (provide 'cofi-mail)

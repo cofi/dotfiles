98feeb98ab5616b223357a80e9ce42718ec746ca
Michael Markert <markert.michael@gmail.com>
2010-07-24 07:33:56 +0200

    [Emacs] Switched to wanderlust.

--- emacs.d/cofi-mail.el
+++ emacs.d/cofi-mail.el
@@ -2,14 +2,15 @@
   (interactive)
   (set-buffer-modified-p nil)
   (setq make-backup-files nil)
-  (mail-mode)
   (turn-on-auto-fill)
   (setq fill-column 72)
-  (ac-flyspell-mode)
-  (mail-text))
+  (ac-flyspell-mode))
 
-;; serve sup
-(push '("\\(compose\\|forward\\|reply\\|resume\\)-mode$" . cofi-do-mail)
-      auto-mode-alist)
+(when (file-exists-p "/usr/local/share/emacs/site-lisp/wl/")
+    (add-to-list 'load-path "/usr/local/share/emacs/site-lisp/wl/"))
+
+(autoload 'wl "wl" "Wanderlust" t)
+(autoload 'wl-other-frame "wl" "Wanderlust on new frame." t)
+(autoload 'wl-draft "wl-draft" "Write draft with Wanderlust." t)
 
 (provide 'cofi-mail)

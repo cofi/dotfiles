9b183bd3ec512b0ecc62d9daccb9286c6ab1b611
Michael Markert <markert.michael@gmail.com>
2010-07-30 21:18:45 +0200

    [Emacs] Extracted public wl-settings from .wl

--- emacs.d/cofi-mail.el
+++ emacs.d/cofi-mail.el
@@ -1,14 +1,7 @@
-(defun cofi-do-mail ()
-  (interactive)
-  (set-buffer-modified-p nil)
-  (setq make-backup-files nil)
-  (turn-on-auto-fill)
-  (setq fill-column 72)
-  (ac-flyspell-mode))
-
 (autoload 'wl "wl" "Wanderlust" t)
 (autoload 'wl-other-frame "wl" "Wanderlust on new frame." t)
 (autoload 'wl-draft "wl-draft" "Write draft with Wanderlust." t)
+(autoload 'wl-user-agent-compose "wl-draft" "Compose mail with Wanderlust" t)
 
 (autoload 'trivial-cite "tc" t t)
 (add-hook 'mail-citation-hook 'trivial-cite)
@@ -50,4 +43,91 @@
                         )
                   )
 
+(if (boundp 'mail-user-agent)
+    (setq mail-user-agent 'wl-user-agent))
+(if (fboundp 'define-mail-user-agent)
+    (define-mail-user-agent
+      'wl-user-agent
+      'wl-user-agent-compose
+      'wl-draft-send
+      'wl-draft-kill
+      'mail-send-hook))
+
+(eval-after-load "wl"
+  '(progn
+     (add-hook 'wl-biff-notify-hook
+               (lambda ()
+                 (send-notification "New Mail!" "Wanderlust")))
+
+     (setq wl-message-ignored-field-list '("^.*:")
+           wl-message-visible-field-list
+           '("^\\(To\\|Cc\\):"
+             "^Subject:"
+             "^\\(From\\|Reply-To\\):"
+             "^Organization:"
+             "^Message-Id:"
+             "^\\(Posted\\|Date\\):"
+             )
+           wl-message-sort-field-list
+           '("^From"
+             "^Organization:"
+             "^Subject"
+             "^Date"
+             "^To"
+             "^Cc"))
+
+     (require-and-exec 'mime-setup
+                       (add-hook 'mime-view-mode-hook
+                                 (lambda ()
+                                   (local-set-key "f" 'browse-url))))
+
+     (setq wl-summary-always-sticky-folder-list t
+           wl-summary-line-format "|%P|%T| %Y/%M/%D %h:%m %t%[%30(%c %f%) %] %s"
+           wl-summary-width 150
+           wl-summary-weekday-name-lang "de"
+           )
+
+     (setq wl-thread-indent-level 2
+           wl-thread-insert-opened t)
+
+     (setq wl-summary-toggle-mime "mime")
+     (require 'mime-w3m)
+     (setq mime-edit-split-message nil)
+
+     (setq wl-forward-subject-prefix "Fwd: " )
+
+     ;; new frame for drafts
+     (setq wl-draft-use-frame t
+           wl-draft-always-delete-myself t
+           )
+     (setq wl-generate-mailer-string-function 'wl-generate-user-agent-string-1)
+     (define-key wl-draft-mode-map (kbd "C-<tab>") 'bbdb-complete-name)
+
+     ;; Proportion of the summary and message windows
+     (setq wl-message-window-size '(3 . 7))
+
+     (add-hook 'wl-mail-setup-hook
+               (lambda ()
+                 (setq make-backup-files nil)
+                 (setq fill-column 72)
+                 (turn-on-auto-fill)
+                 (ac-flyspell-mode)
+                 ))
+
+     ;; Invert behaviour of with and without argument replies.
+     (setq wl-draft-reply-without-argument-list
+           '(("Reply-To" ("Reply-To") nil nil)
+             ("Mail-Reply-To" ("Mail-Reply-To") nil nil)
+             ("From" ("From") nil nil)))
+
+     (setq wl-draft-reply-with-argument-list
+           '(("Followup-To" nil nil ("Followup-To"))
+             ("Mail-Followup-To" ("Mail-Followup-To") nil ("Newsgroups"))
+             ("Reply-To" ("Reply-To") ("To" "Cc" "From") ("Newsgroups"))
+             ("From" ("From") ("To" "Cc") ("Newsgroups"))))
+
+     ;; Switch reply keys
+     (define-key wl-summary-mode-map (kbd "A") 'wl-summary-reply)
+     (define-key wl-summary-mode-map (kbd "a") 'wl-summary-reply-with-citation)
+     ))
 (provide 'cofi-mail)

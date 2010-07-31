ef79f53dd4302995ce092fec28109548eec3bc9d
Michael Markert <markert.michael@gmail.com>
2010-08-01 00:38:56 +0200

    [Emacs] Fixed yas-settings.
    And removed fallback to my buggy tab. I really should take care of it ...

--- emacs.d/cofi-snippets.el
+++ emacs.d/cofi-snippets.el
@@ -1,13 +1,10 @@
 (add-to-list 'load-path "~/.elisp/vendor/yasnippet")
 (require-and-exec 'yasnippet
-    (setq yas/snippet-dirs '(
-                             "~/.emacs.d/snippets"
-                             "~/.elisp/vendor/yasnippet/snippets"
-                             ))
     (yas/initialize)
+    (yas/load-directory "~/.elisp/vendor/yasnippet/snippets")
+    (yas/load-directory "~/.emacs.d/snippets")
     (setq yas/prompt-functions '(yas/ido-prompt
                                  yas/completing-prompt))
-    (setq yas/fallback-behavior '(apply smart-tab))
     (setq yas/also-auto-indent-first-line t)
 
     (global-set-key (kbd "M-RET") 'yas/expand)

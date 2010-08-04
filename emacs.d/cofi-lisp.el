85eb3a04fef6dcc1966d105f426cb54e58abc676
Michael Markert <markert.michael@gmail.com>
2010-08-04 04:45:07 +0200

    [Emacs] Added `' pairs for elisp.

--- emacs.d/cofi-lisp.el
+++ emacs.d/cofi-lisp.el
@@ -1,3 +1,11 @@
+;; Add ` '-Pair for elisp
+(add-hook 'emacs-lisp-mode-hook
+           #'(lambda ()
+               (push '(?` . ?')
+                     (getf autopair-extra-pairs :comment))
+               (push '(?` . ?')
+                     (getf autopair-extra-pairs :string))))
+
 (add-hook 'lisp-mode-hook 
           (lambda () (local-set-key (kbd "RET") 'newline-and-indent))) 
 

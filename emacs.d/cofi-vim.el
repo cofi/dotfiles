da1438474f4fd009b0b8fac104c665fde496c8d4
Michael Markert <markert.michael@gmail.com>
2010-08-07 02:21:45 +0200

    [Emacs] Updated sackspace.

--- emacs.d/cofi-vim.el
+++ emacs.d/cofi-vim.el
@@ -60,8 +60,7 @@ Vanilla in vi-state; Prefixed witf `C-' in insert-state")
 
   ;; Vim-like backspace (backspace=indent,eol,start) ==========
   (require-and-exec 'sackspace
-    (define-key viper-insert-global-user-map (kbd "<backspace>") 'sack/backspace)
-    (define-key viper-insert-global-user-map (kbd "C-<backspace>") 'sack/hyper-sack))
+                    (sack/install-in-viper))
   ;; ==================================================
 
   ;; Search keybindings ========================================

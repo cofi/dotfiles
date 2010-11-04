f2ee0db7ce7dd20ab02812d69f86873e822c21bf
Michael Markert <markert.michael@gmail.com>
2010-11-05 00:47:45 +0100

    [Emacs] Added tab-stop-list.

--- emacs.d/cofi-ui.el
+++ emacs.d/cofi-ui.el
@@ -187,7 +187,10 @@
 (setq speck-doublet t)
 
 ;; tab settings
-(setq-default indent-tabs-mode nil     ; no nasty tabs i say!
+
+(setq-default tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68
+                              72 76 80 84 88 92 96 100 104 108 112 116 120)
+              indent-tabs-mode nil     ; no nasty tabs i say!
               tab-width 4)
 
 (setq dabbrev-case-replace nil)

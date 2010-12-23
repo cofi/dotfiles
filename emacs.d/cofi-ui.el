de6275e34cb5a5d580b62ba61edb302d336eb156
Michael Markert <markert.michael@gmail.com>
2010-12-23 20:38:46 +0100

    [Emacs] Added save place.

--- emacs.d/cofi-ui.el
+++ emacs.d/cofi-ui.el
@@ -218,6 +218,8 @@
 ;; Default modes ========================================
 (require-and-exec 'autopair
                   (autopair-global-mode 1))
+(require-and-exec 'saveplace
+                  (setq-default save-place t))
 (transient-mark-mode t)
 (display-time-mode t)
 (global-font-lock-mode t)

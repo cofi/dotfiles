5d4dd1f7b3fe34d6dbbc18b5d68d12c7cc71211a
Michael Markert <markert.michael@googlemail.com>
2011-01-07 21:12:46 +0100

    [Emacs] Added paredit.

--- emacs.d/cofi-lisp.el
+++ emacs.d/cofi-lisp.el
@@ -10,6 +10,12 @@
             (local-set-key (kbd "C-c C-c") 'eval-buffer)
             (setq mode-name "eL")))
 
+(require-and-exec 'paredit
+  (dolist (hook '(lisp-interaction-mode-hook emacs-lisp-mode-hook
+                  lisp-mode-hook clojure-mode-hook))
+    (add-hook hook #'(lambda () (setq autopair-dont-activate t)))
+    (add-hook hook 'enable-paredit-mode t)))
+
 (define-key read-expression-map (kbd "TAB") 'lisp-complete-symbol)
 
 (provide 'cofi-lisp)

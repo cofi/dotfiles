a0b51c539fc38d97b323737bddbfded2654e7d3a
Michael Markert <markert.michael@googlemail.com>
2011-01-10 08:00:02 +0100

    [Emacs] Changed lisp mode hooks.

--- emacs.d/cofi-lisp.el
+++ emacs.d/cofi-lisp.el
@@ -10,14 +10,26 @@
             (local-set-key (kbd "C-c C-c") 'eval-buffer)
             (setq mode-name "eL")))
 
+(dolist (hook '(clojure-mode-hook lisp-mode-hook))
+  (add-hook hook
+            ;; viper overshadows slime-repl binding
+            (lambda () (local-set-key (kbd "C-c M-p") 'slime-repl-set-package))))
+
+(add-hook 'slime-repl-mode-hook
+          (lambda () (local-set-key (kbd "C-c C-z") 'other-buffer)))
+
+(defun subst-autopair-with-paredit ()
+  (interactive)
+  (setq autopair-dont-activate t)
+  (enable-paredit-mode))
+
 (require-and-exec 'paredit
   (dolist (hook '(lisp-interaction-mode-hook
                   emacs-lisp-mode-hook
                   lisp-mode-hook
                   clojure-mode-hook
                   slime-repl-mode-hook))
-    (add-hook hook #'(lambda () (setq autopair-dont-activate t)))
-    (add-hook hook 'enable-paredit-mode t)))
+    (add-hook hook 'subst-autopair-with-paredit)))
 
 (add-to-list 'auto-mode-alist '("\\.cl$" . lisp-mode))
 
@@ -31,7 +43,7 @@
 
 (require-and-exec 'slime
      (require 'slime-autoloads)
-     (slime-setup))
+     (slime-setup '(slime-repl)))
 
 (define-key read-expression-map (kbd "TAB") 'lisp-complete-symbol)
 

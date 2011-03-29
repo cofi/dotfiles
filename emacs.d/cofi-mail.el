963fa636ae5f50361686010ee287715fd7b81a85
Michael Markert <markert.michael@googlemail.com>
2011-03-30 00:34:37 +0200

    [Emacs] Moved wl settings out of eval-after-load.

--- emacs.d/cofi-mail.el
+++ emacs.d/cofi-mail.el
@@ -52,77 +52,63 @@
   (auto-dictionary-mode)
   (turn-on-flyspell))
 
+(add-hook 'mime-view-mode-hook
+          (lambda ()
+            (local-set-key "f" 'browse-url)
+            (local-set-key "F" 'w3m-view-url-with-external-browser)))
+
+(add-hook 'wl-mail-send-pre-hook 'mail-attachment-check)
+(add-hook 'wl-mail-send-pre-hook 'mail-subject-check)
+(add-hook 'wl-biff-notify-hook
+          (lambda ()
+            (x-urgency-hint (selected-frame) t)
+            (send-notification "New Mail!" "Wanderlust")))
+(setq wl-message-ignored-field-list '("^.*:")
+      wl-message-visible-field-list
+      '("^\\(To\\|Cc\\):"
+        "^Subject:"
+        "^\\(From\\|Reply-To\\):"
+        "^Organization:"
+        "^Message-Id:"
+        "^\\(Posted\\|Date\\):"
+        )
+      wl-message-sort-field-list
+      '("^From"
+        "^Organization:"
+        "^Subject"
+        "^Date"
+        "^To"
+        "^Cc"))
+(setq wl-summary-line-format "|%P|%T| %Y/%M/%D %h:%m %t%[%30(%c %f%) %] %s"
+      wl-summary-width nil
+      wl-summary-weekday-name-lang "de"
+      wl-summary-toggle-mime "mime")
+
+(setq wl-thread-indent-level 2
+      wl-thread-insert-opened t)
+(setq wl-forward-subject-prefix "Fwd: " )
+(setq wl-draft-use-frame t
+      wl-draft-always-delete-myself t
+      wl-auto-save-drafts-interval nil)
+(setq mime-edit-split-message nil)
+(setq wl-generate-mailer-string-function 'wl-generate-user-agent-string-1)
+;; org-contacts
+(add-hook 'wl-draft-mode-hook
+          (lambda ()
+            (add-to-list 'completion-at-point-functions
+                         'org-contacts-message-complete-function)))
+;; Proportion of the summary and message windows
+(setq wl-message-window-size '(3 . 7))
+(add-hook 'wl-mail-setup-hook 'cofi/write-mail)
 (eval-after-load "wl"
   '(progn
-     (add-hook 'wl-mail-send-pre-hook 'mail-attachment-check)
-     (add-hook 'wl-mail-send-pre-hook 'mail-subject-check)
-     
-     (add-hook 'wl-biff-notify-hook
-               (lambda ()
-                 (x-urgency-hint (selected-frame) t)
-                 (send-notification "New Mail!" "Wanderlust")))
-
-     (setq wl-message-ignored-field-list '("^.*:")
-           wl-message-visible-field-list
-           '("^\\(To\\|Cc\\):"
-             "^Subject:"
-             "^\\(From\\|Reply-To\\):"
-             "^Organization:"
-             "^Message-Id:"
-             "^\\(Posted\\|Date\\):"
-             )
-           wl-message-sort-field-list
-           '("^From"
-             "^Organization:"
-             "^Subject"
-             "^Date"
-             "^To"
-             "^Cc"))
-
-     (require-and-exec 'mime-setup
-                       (add-hook 'mime-view-mode-hook
-                                 (lambda ()
-                                   (local-set-key "f" 'browse-url)
-                                   (local-set-key "F" 'w3m-view-url-with-external-browser))))
-
-     (setq wl-summary-always-sticky-folder-list t
-           wl-summary-line-format "|%P|%T| %Y/%M/%D %h:%m %t%[%30(%c %f%) %] %s"
-           wl-summary-width 150
-           wl-summary-weekday-name-lang "de"
-           )
-
-     (setq wl-thread-indent-level 2
-           wl-thread-insert-opened t)
-
-     (setq wl-summary-toggle-mime "mime")
      (require 'mime-w3m)
-     (setq mime-edit-split-message nil)
-
-     (setq wl-forward-subject-prefix "Fwd: " )
-
-     (setq wl-draft-use-frame t
-           wl-draft-always-delete-myself t
-           wl-auto-save-drafts-interval nil
-           )
-     (setq wl-generate-mailer-string-function 'wl-generate-user-agent-string-1)
-     (define-key wl-draft-mode-map (kbd "C-<tab>") 'bbdb-complete-name)
-
-     ;; Proportion of the summary and message windows
-     (setq wl-message-window-size '(3 . 7))
-
-     (add-hook 'wl-mail-setup-hook 'cofi/write-mail)
-
+     (define-key wl-draft-mode-map (kbd "C-<tab>") 'completion-at-point)
      ;; Invert behaviour of with and without argument replies.
-     (setq wl-draft-reply-without-argument-list
-           '(("Reply-To" ("Reply-To") nil nil)
-             ("Mail-Reply-To" ("Mail-Reply-To") nil nil)
-             ("From" ("From") nil nil)))
-
-     (setq wl-draft-reply-with-argument-list
-           '(("Followup-To" nil nil ("Followup-To"))
-             ("Mail-Followup-To" ("Mail-Followup-To") nil ("Newsgroups"))
-             ("Reply-To" ("Reply-To") ("To" "Cc" "From") ("Newsgroups"))
-             ("From" ("From") ("To" "Cc") ("Newsgroups"))))
+     (let ((with-arg wl-draft-reply-without-argument-list)
+           (without-arg wl-draft-reply-with-argument-list))
+       (setq wl-draft-reply-without-argument-list without-arg
+             wl-draft-reply-with-argument-list with-arg))
 
      ;; Switch reply keys
      (define-key wl-summary-mode-map (kbd "A") 'wl-summary-reply)
@@ -159,20 +145,18 @@
         (wl-folder-goto-folder-subr
          (concat mairix-wl-search-folder-prefix (match-string 1 folder)))))
 
-
-    (defun mairix-wl-fetch-field (field)
-      "Get mail header FIELD for current message using Wanderlust."
-      (when wl-summary-buffer-elmo-folder
-        (elmo-message-field
-         wl-summary-buffer-elmo-folder
-         (wl-summary-message-number)
-         (intern (downcase field)))))
-
     (add-to-list 'mairix-display-functions '(wl mairix-wl-display))
     (add-to-list 'mairix-get-mail-header-functions '(wl mairix-wl-fetch-field))
 
     (eval-after-load "wl"
       '(progn
+         (defun mairix-wl-fetch-field (field)
+           "Get mail header FIELD for current message using Wanderlust."
+           (when wl-summary-buffer-elmo-folder
+             (elmo-message-field
+              wl-summary-buffer-elmo-folder
+              (wl-summary-message-number)
+              (intern (downcase field)))))
          (define-key wl-folder-mode-map (kbd "C-/") 'mairix-search)
          (define-key wl-summary-mode-map (kbd "C-/") 'mairix-search)))
 

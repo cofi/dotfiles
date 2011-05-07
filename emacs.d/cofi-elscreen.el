1972c8cc76cd3aed7657dfdb552125360b71fb9c
Michael Markert <markert.michael@googlemail.com>
2011-05-07 20:46:25 +0200

    [Emacs] Create automatically new elscreens.

--- emacs.d/cofi-elscreen.el
+++ emacs.d/cofi-elscreen.el
@@ -3,5 +3,22 @@
 (require 'elscreen)
 (require 'elscreen-dired)
 (require 'elscreen-server)
+;;; in cofi-mail.el: elscreen-wl
+
+(defmacro elscreen-create-automatically (ad-do-it)
+  `(if (not (elscreen-one-screen-p))
+       ,ad-do-it
+     (elscreen-create)
+     (elscreen-notify-screen-modification 'force-immediately)
+     (elscreen-message "New screen is automatically created")))
+
+(defadvice elscreen-next (around elscreen-create-automatically activate)
+  (elscreen-create-automatically ad-do-it))
+
+(defadvice elscreen-previous (around elscreen-create-automatically activate)
+  (elscreen-create-automatically ad-do-it))
+
+(defadvice elscreen-toggle (around elscreen-create-automatically activate)
+  (elscreen-create-automatically ad-do-it))
 
 (provide 'cofi-elscreen)

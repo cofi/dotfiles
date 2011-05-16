46ab943cb85f8b11063e6b9764b93d7098c823db
Michael Markert <markert.michael@googlemail.com>
2011-05-16 22:45:37 +0200

    [Emacs] Add midnight/clean buffer settings.

--- emacs.d/cofi-buffer.el
+++ emacs.d/cofi-buffer.el
@@ -95,4 +95,10 @@
   (global-set-key (kbd "C-x C-b") 'ibuffer-other-window)
   )
 
+(require 'midnight)
+(setq midnight-period (* 3600 6))       ; every 6 hours
+
+(setq clean-buffer-list-delay-general 2           ; every 2 day
+      clean-buffer-list-delay-special (* 3600 3)) ; every 3 hours
+
 (provide 'cofi-buffer)

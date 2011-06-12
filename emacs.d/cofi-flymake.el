cb0f4dd9cd111661abbd9439c8795afc7586c8ec
Michael Markert <markert.michael@googlemail.com>
2011-06-12 16:54:24 +0200

    [Emacs] Add flymake-cursor.

--- emacs.d/cofi-flymake.el
+++ emacs.d/cofi-flymake.el
@@ -1,19 +1,7 @@
-(require-and-exec 'flymake
-  ;; check only after save
-  (setq flymake-no-changes-timeout 9999
-        flymake-start-syntax-check-on-newline nil)
-
-  (defun my-flymake-show-help ()
-    "Display flymake message at point.
-Useful in a tty without mouse-over.
-Intended to be run from a local `post-command-hook'."
-    (when (get-char-property (point) 'flymake-overlay)
-      (let ((help (get-char-property (point) 'help-echo)))
-        (if help (message "%s" help)))))
-
-  (add-hook 'flymake-mode-hook
-            '(lambda ()
-               (add-hook 'post-command-hook 'my-flymake-show-help nil t)))
-  )
+;; check only after save
+(setq flymake-no-changes-timeout 9999
+      flymake-start-syntax-check-on-newline nil)
+(require 'flymake)
+(require 'flymake-cursor)
 
 (provide 'cofi-flymake)

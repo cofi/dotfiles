7a892f20f742ca54604046694df3f866403eb21a
Michael Markert <markert.michael@googlemail.com>
2011-06-18 02:27:56 +0200

    [Emacs] Add magit settings.
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- emacs.d/cofi-vcs.el
+++ emacs.d/cofi-vcs.el
@@ -7,8 +7,14 @@
 (setq vc-handled-backends '(SVN)
       vc-follow-symlinks t)
 
+;;; (ma)git
 (global-set-key (kbd "C-c g") 'magit-status)
 (vim-mapleader-add "g" 'magit-status)
+(setq magit-commit-signoff t
+      magit-completing-read-function 'magit-ido-completing-read
+      magit-commit-all-when-nothing-staged nil)
+
+;;; (a)hg
 (require 'ahg)
 (setq ahg-global-key-prefix (kbd "C-c h"))
 (vim-mapleader-add "h" 'ahg-status)

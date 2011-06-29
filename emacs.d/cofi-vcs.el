1fba39c3b852b3cd4b85ef4be530a8d96e6ba41f
Michael Markert <markert.michael@googlemail.com>
2011-06-30 00:10:34 +0200

    [Emacs] Override `magit-quit-window'.
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- emacs.d/cofi-vcs.el
+++ emacs.d/cofi-vcs.el
@@ -14,6 +14,17 @@
       magit-completing-read-function 'magit-ido-completing-read
       magit-commit-all-when-nothing-staged nil)
 
+;;; overwrite magit-quit-window with a more sensible fun
+(defun cofi/magit-quit-window (&optional kill-buffer)
+  (interactive "P")
+  (if (one-window-p)
+      (if kill-buffer
+          (kill-buffer (current-buffer))
+        (bury-buffer))
+    (quit-window kill-buffer (selected-window))))
+(eval-after-load "magit"
+  '(fset 'magit-quit-window #'cofi/magit-quit-window))
+
 ;;; (a)hg
 (require 'ahg)
 (setq ahg-global-key-prefix (kbd "C-c h"))

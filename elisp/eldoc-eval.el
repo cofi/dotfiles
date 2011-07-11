84f178982c75ce8bdcd6786570e8c6c596298e57
Michael Markert <markert.michael@googlemail.com>
2011-07-11 22:37:35 +0200

    [Emacs] Update eldoc-eval.
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- elisp/eldoc-eval.el
+++ elisp/eldoc-eval.el
@@ -24,48 +24,109 @@
 ;; Code:
 (require 'eldoc)
 
-(defvar eldoc-in-minibuffer t)
-(defvar eval-prefered-function 'pp-eval-expression)
-(defvar eldoc-in-minibuffer-show-fn 'eldoc-show-in-mode-line
+;;; Minibuffer support.
+;;  Enable displaying eldoc info in something else
+;;  Than minibuffer when this one is in use.
+;;
+(defcustom eldoc-in-minibuffer t
+  "Turn on eldoc in minibuffer."
+  :group 'eldoc
+  :type 'bolean)
+
+(defcustom eldoc-in-minibuffer-show-fn 'eldoc-show-in-mode-line
   "A function to display eldoc info.
-Should take one arg: the string to display")
-(defvar eldoc-show-in-mode-line-delay 12)
+Should take one arg: the string to display"
+  :group 'eldoc
+  :type 'function)
+
+(defcustom eldoc-show-in-mode-line-delay 12
+  "The time we show eldoc when emacs is idle."
+  :group 'eldoc
+  :type 'number)
+
+(defcustom eval-prefered-function 'pp-eval-expression
+  "prefered function to use with `M-:'."
+  :group 'lisp
+  :type 'function)
+
+;; Internal.
+(defvar eldoc-active-minibuffers-list nil
+  "Store actives minibuffers with eldoc enabled.")
+
+(defun eldoc-store-minibuffer ()
+  "Store minibuffer buffer name in `eldoc-active-minibuffers-list'.
+This function is called by each minibuffer started with eldoc support.
+See `with-eldoc-in-minibuffer'."
+  (with-selected-window (minibuffer-window)
+    (push (buffer-name) eldoc-active-minibuffers-list)))
+
+(defmacro with-eldoc-in-minibuffer (&rest body)
+  "Enable eldoc support for minibuffer input that run in BODY."
+  (declare (indent 0) (debug t))
+  `(let ((timer (and eldoc-in-minibuffer
+                     (run-with-idle-timer
+                      eldoc-idle-delay
+                      'repeat 'eldoc-mode-in-minibuffer))))
+     (unwind-protect
+         (minibuffer-with-setup-hook
+             ;; When minibuffer is activated in body,
+             ;; store it.
+             'eldoc-store-minibuffer
+           ,@body)
+       (and timer (cancel-timer timer))
+       ;; Each time a minibuffer exit or abort
+       ;; his buffer is removed from stack,
+       ;; assuming we can only exit the active minibuffer
+       ;; on top of stack.
+       (setq eldoc-active-minibuffers-list
+             (cdr eldoc-active-minibuffers-list)))))
 
 (defun eldoc-show-in-mode-line (str)
-  (with-current-buffer (current-buffer)
-    (let ((mode-line-format (concat " " str)))
-      (force-mode-line-update)
-      (sit-for eldoc-show-in-mode-line-delay))
-    (force-mode-line-update)))
+  "Display string STR in the mode-line next to minibuffer."
+  (let (mode-line-in-non-selected-windows)
+    (with-current-buffer (window-buffer
+                          (with-selected-frame (selected-frame)
+                            (if (fboundp 'window-in-direction)
+                                (window-in-direction ; Only emacs24
+                                 'above (minibuffer-window))
+                                (minibuffer-selected-window))))
+      (make-local-variable 'mode-line-format)
+      (let ((mode-line-format (concat " " str)))
+        (force-mode-line-update nil)
+        (sit-for eldoc-show-in-mode-line-delay))
+      (force-mode-line-update))))
 
-(defun eldoc-show-in-eval ()
+(defun eldoc-mode-in-minibuffer ()
   "Show eldoc for current minibuffer input."
-  (interactive)
-  (let* ((str-all (minibuffer-completion-contents))
-         (sym     (when str-all
-                    (with-temp-buffer
-                      (insert str-all)
-                      (goto-char (point-max))
-                      (unless (looking-back ")\\|\"") (forward-char -1))
-                      (eldoc-current-symbol))))
-         (doc     (or (eldoc-get-var-docstring sym)
-                      (eldoc-get-fnsym-args-string
-                       (car (eldoc-fnsym-in-current-sexp))))))
-    (when doc (funcall eldoc-in-minibuffer-show-fn doc))))
+  (let ((buf (with-selected-window (minibuffer-window)
+               (buffer-name))))
+    ;; If this minibuffer have been started with
+    ;;`with-eldoc-in-minibuffer' give it eldoc support
+    ;; and update mode-line, otherwise do nothing.
+    (when (member buf eldoc-active-minibuffers-list)
+      (let* ((str-all (with-current-buffer buf
+                        (minibuffer-completion-contents)))
+             (sym     (when str-all
+                        (with-temp-buffer
+                          (insert str-all)
+                          (goto-char (point-max))
+                          (unless (looking-back ")\\|\"")
+                            (forward-char -1))
+                          (eldoc-current-symbol))))
+             (info-fn (eldoc-fnsym-in-current-sexp))
+             (doc     (or (eldoc-get-var-docstring sym)
+                          (eldoc-get-fnsym-args-string
+                           (car info-fn) (cadr info-fn)))))
+        (when doc (funcall eldoc-in-minibuffer-show-fn doc))))))
 
-(defun eldoc-eval-expression ()
-  "`eval-expression' with eldoc support."
+(defun eval-expression-with-eldoc ()
+  "Eval expression with eldoc support in mode-line."
   (interactive)
-  (if eldoc-in-minibuffer
-      (let ((timer (run-with-idle-timer eldoc-idle-delay
-                                        'repeat 'eldoc-show-in-eval)))
-        (unwind-protect
-             (call-interactively eval-prefered-function)
-          (cancel-timer timer)))
-      (call-interactively eval-prefered-function)))
+  (with-eldoc-in-minibuffer
+    (call-interactively eval-prefered-function)))
 
-(global-set-key [remap eval-expression] 'eldoc-eval-expression)
+;; Bind it to `M-:'.
+(global-set-key [remap eval-expression] 'eval-expression-with-eldoc)
 
 (provide 'eldoc-eval)
-
-;; eldoc-eval.el ends here
+;;; eldoc-eval.el -- Show eldoc when using M-:

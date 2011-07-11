a50714939639ea64406a4b09edd039b53905c288
Michael Markert <markert.michael@googlemail.com>
2011-07-11 22:36:13 +0200

    [Xmonad] Add rfc search.
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- xmonad.hs
+++ xmonad.hs
@@ -323,6 +323,7 @@ searchBindings = [("M-S-/ " ++ key, S.selectSearch engine) | (key, engine) <- se
                                                   , S.alpha
                                                   , mathworld
                                                   , ctan
+                                                  , rfc
                                                   , S.prefixAware google
                                                   ]
       -- new ones
@@ -345,6 +346,7 @@ searchBindings = [("M-S-/ " ++ key, S.selectSearch engine) | (key, engine) <- se
       urbanDictionary = S.searchEngine "ud" "http://www.urbandictionary.com/define.php?term="
       ctan = S.searchEngine "ctan" "http://www.ctan.org/search/?search_type=description&search_type=filename&search_type=id&search="
       duck = S.searchEngine "d" "http://duckduckgo.com/?q="
+      rfc = S.searchEngine "rfc" "http://www.ietf.org/rfc/rfc"
       -- new names
       mathworld = S.namedEngine "math" S.mathworld
       code = S.namedEngine "code" S.codesearch

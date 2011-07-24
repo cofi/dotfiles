fa2388dcbf9adb439a83ca89fd2c712493b4646e
Michael Markert <markert.michael@googlemail.com>
2011-07-24 16:00:42 +0200

    [Emacs] Fix <home> in shell mode.
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- emacs.d/cofi-shell.el
+++ emacs.d/cofi-shell.el
@@ -32,5 +32,8 @@
 
 (setq dirtrack-list '("[a-zA-Z@]+ \\(~.*?\\) %>" 1))
 (add-hook 'shell-mode-hook (turn-on dirtrack-mode))
+(add-hook 'shell-mode-hook
+          (gen-local-fill-keymap-hook
+              "<home>" 'comint-bol))
 
 (provide 'cofi-shell)

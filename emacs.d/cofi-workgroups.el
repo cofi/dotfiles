015a67754926fa6995da473cc05b6ffce1ddcda2
Michael Markert <markert.michael@googlemail.com>
2011-08-03 23:31:15 +0200

    [Emacs] workgroups: Don't morph.
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- emacs.d/cofi-workgroups.el
+++ emacs.d/cofi-workgroups.el
@@ -7,6 +7,7 @@
       wg-mode-line-left-brace "{"
       wg-mode-line-right-brace "} "
       wg-query-for-save-on-emacs-exit nil
-      wg-file (cofi/var-file "emacs/wg"))
+      wg-file (cofi/var-file "emacs/wg")
+      wg-morph-on nil)
 
 (provide 'cofi-workgroups)

b85926fb0451c6ef347911612564d20ef5d74325
Michael Markert <markert.michael@googlemail.com>
2011-08-04 19:37:57 +0200

    [Emacs] Fix markup config.
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- emacs.d/cofi-markup.el
+++ emacs.d/cofi-markup.el
@@ -2,8 +2,8 @@
                turn-on-auto-fill
                ,(turn-on auto-dictionary-mode))))
 
-  (add-all-to-hook 'rst-mode-hook modes)
+  (add-to-hook 'rst-mode-hook modes)
 
-  (add-all-to-hook 'markdown-mode-hook modes))
+  (add-to-hook 'markdown-mode-hook modes))
 
 (provide 'cofi-markup)

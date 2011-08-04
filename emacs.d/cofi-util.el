1bc14d69e036ae5a17f62f7b64fd4ed8cba9571d
Michael Markert <markert.michael@googlemail.com>
2011-08-04 19:37:49 +0200

    [Emacs] Add `add-to-hook' convenience fun.
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- emacs.d/cofi-util.el
+++ emacs.d/cofi-util.el
@@ -60,7 +60,11 @@
     (add-hook hook fun)))
 
 (defun add-all-to-hook (hook &rest funs)
-  "Add functions to hook"
+  "Add functions to hook."
+  (add-to-hook hook funs))
+
+(defun add-to-hook (hook funs)
+  "Add list of functions to hook."
   (dolist (fun funs)
     (add-hook hook fun)))
 

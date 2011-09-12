9d8f20dcb26d3442004bec4cbebaf9a4ec1cd9d2
Michael Markert <markert.michael@googlemail.com>
2011-09-12 15:53:37 +0200

    [Emacs] Autoload eclim stuff.
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- emacs.d/cofi-java.el
+++ emacs.d/cofi-java.el
@@ -1,10 +1,9 @@
 (add-to-loadpath "~/.elisp/vendor/emacs-eclim"
                  "~/.elisp/vendor/emacs-eclim/vendor/")
 
-(require-and-exec 'eclim
-                  (add-hook 'java-mode-hook 'eclim-mode)
-                  (setq eclim-auto-save t
-                        eclim-use-yasnippet nil)
-                  )
+(setq eclim-auto-save t
+      eclim-use-yasnippet nil)
+(autoload 'eclim-mode "eclim" nil t)
+(add-hook 'java-mode-hook 'eclim-mode)
 
 (provide 'cofi-java)

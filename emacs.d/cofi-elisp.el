d1a2df6ac197f31fe23d68dd0b9bd3ac7c439c81
Michael Markert <markert.michael@googlemail.com>
2011-10-05 13:14:25 +0200

    [Emacs] Repl binding for elisp.
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- emacs.d/cofi-elisp.el
+++ emacs.d/cofi-elisp.el
@@ -12,7 +12,9 @@
 
 (add-hook 'emacs-lisp-mode-hook
           (gen-fill-keymap-hook emacs-lisp-mode-map
-                                "C-c C-c" 'eval-buffer))
+                                "C-c C-c" 'eval-buffer
+                                "C-c C-r" 'ielm))
+
 (add-hook 'ielm-mode-hook 'eldoc-mode)
 
 (require 'eldoc-eval)

60b00fcda7ecced55b1ea7b28e759ce71328b041
Michael Markert <markert.michael@googlemail.com>
2011-12-10 01:08:13 +0100

    [Emacs] python: move to fgallina's python-mode.
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- emacs.d/cofi-python.el
+++ emacs.d/cofi-python.el
@@ -1,3 +1,13 @@
+(add-to-loadpath "~/.elisp/vendor/python")
+;;; setup for ipython 0.11
+(setq python-shell-interpreter "ipython"
+      python-shell-interpreter-args ""
+      python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+      python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+      python-shell-completion-setup-code "from IPython.core.completerlib import module_completion"
+      python-shell-completion-module-string-code "';'.join(module_completion('''%s'''))\n"
+      python-shell-completion-string-code "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+
 (require-and-exec 'flymake
   (defun flymake-pylint-init ()
     (let* ((temp-file (flymake-init-create-temp-buffer-copy
@@ -12,8 +22,6 @@
 
 ;; Folding
 (add-hook 'python-mode-hook 'hs-minor-mode)
-
-(setq python-python-command "ipython")
 
 ;; setup pymacs
 (autoload 'pymacs-apply "pymacs")

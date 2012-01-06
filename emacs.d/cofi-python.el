13c376e2ec57f8ddd26be69a628a25202ccb7df7
Michael Markert <markert.michael@googlemail.com>
2012-01-06 18:50:00 +0100

    [Emacs] Add pydoc-info.
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- emacs.d/cofi-python.el
+++ emacs.d/cofi-python.el
@@ -84,5 +84,15 @@
                            "u"   'rope-undo
                            "C-r" 'rope-redo)))
 
-(add-hook 'python-mode-hook 'highlight-80+-mode))
+(add-hook 'python-mode-hook 'highlight-80+-mode)
+
+(require-and-exec 'pydoc-info
+  (require 'info-look)
+  (info-lookup-add-help
+   :mode 'python-mode
+   :parse-rule 'pydoc-info-python-symbol-at-point
+   :doc-spec
+   '(("(python)Index" pydoc-info-lookup-transform-entry)
+     ("(sphinx)Index" pydoc-info-lookup-transform-entry))))
+
 (provide 'cofi-python)

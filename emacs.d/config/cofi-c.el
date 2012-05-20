0217727c96dacca9a405afe596aa9b784c6c68c6
Michael Markert <markert.michael@googlemail.com>
2012-05-20 21:37:52 +0200

    [Emacs] Add maybe-change-c-to-c++.
    For changing header files to c++ mode when it is more appropriate.
    
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- emacs.d/config/cofi-c.el
+++ emacs.d/config/cofi-c.el
@@ -1,3 +1,5 @@
+(require 'cofi-util)
+
 (setq-default c-basic-offset 4)
 (setq c-default-style '((java-mode . "java")
                         (awk-mode . "awk")
@@ -10,6 +12,17 @@
   (setq comment-start "//"
         comment-end ""))
 (add-hook 'c-mode-hook 'cofi-c-line-comments)
+
+(defun cofi/maybe-change-c-to-c++ ()
+  (when (and (buffer-file-name) (ends-with (buffer-file-name) ".h"))
+    (let* ((name (file-name-sans-extension (file-name-nondirectory (buffer-file-name))))
+           (siblings (ls-files "." nil name)))
+      (when (find-if (lambda (f)
+                       (string-match (gen-extension-re "cc" "c++" "cpp" "cxx") (downcase f)))
+                     siblings)
+        (c++-mode)))))
+
+(add-hook 'c-mode-hook #'cofi/maybe-change-c-to-c++)
 
 (require-and-exec 'c-eldoc
   (add-hook 'c-mode-hook 'c-turn-on-eldoc-mode))

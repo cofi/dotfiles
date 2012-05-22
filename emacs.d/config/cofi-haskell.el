86600a803d98b4b005deb6a2886f654b0379a67b
Michael Markert <markert.michael@googlemail.com>
2012-05-22 23:07:59 +0200

    [Emacs] haskell: fix mode hook.
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- emacs.d/config/cofi-haskell.el
+++ emacs.d/config/cofi-haskell.el
@@ -2,8 +2,8 @@
                  "~/.elisp/ghc-mod/")
 
 (load-and-exec "haskell-site-file"
-  (add-all-to-hook 'haskell-mode-hook '(haskell-doc-mode
-                                        haskell-indentation-mode)))
+  (add-all-to-hook 'haskell-mode-hook 'haskell-doc-mode 'haskell-indentation-mode))
+
 (require-and-exec 'ghc
   (add-hook 'haskell-mode-hook 'ghc-init))
 

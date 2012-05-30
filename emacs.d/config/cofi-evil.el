ff0fffcc3d53c3b14a6c357b0d0f095d27e80da3
Michael Markert <markert.michael@googlemail.com>
2012-05-30 19:43:04 +0200

    [Emacs] evil: Change state tags (uppercase & visual tag).
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- emacs.d/config/cofi-evil.el
+++ emacs.d/config/cofi-evil.el
@@ -15,11 +15,12 @@
 (require 'evil-leader)
 (require 'evil)
 
-(setq evil-normal-state-tag   (propertize "n" 'face '((:background "green" :foreground "black")))
-      evil-emacs-state-tag    (propertize "e" 'face '((:background "orange" :foreground "black")))
-      evil-insert-state-tag   (propertize "i" 'face '((:background "red")))
-      evil-motion-state-tag   (propertize "m" 'face '((:background "blue")))
-      evil-operator-state-tag (propertize "o" 'face '((:background "purple"))))
+(setq evil-normal-state-tag   (propertize "N" 'face '((:background "green" :foreground "black")))
+      evil-emacs-state-tag    (propertize "E" 'face '((:background "orange" :foreground "black")))
+      evil-insert-state-tag   (propertize "I" 'face '((:background "red")))
+      evil-motion-state-tag   (propertize "M" 'face '((:background "blue")))
+      evil-visual-state-tag   (propertize "V" 'face '((:background "grey80" :foreground "black")))
+      evil-operator-state-tag (propertize "O" 'face '((:background "purple"))))
 
 ;;; evil-surround
 (require-and-exec 'surround

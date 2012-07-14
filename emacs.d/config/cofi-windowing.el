626cc86c36fffc5427deedd1b8f9041a598a4077
Michael Markert <markert.michael@googlemail.com>
2012-07-14 20:25:25 +0200

    [Emacs] Override `winner-set` with a version that accepts also RE strings.
    No, thank you, but I don't want to provide an exhaustive list of f**king boring buffers ...
    
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- emacs.d/config/cofi-windowing.el
+++ emacs.d/config/cofi-windowing.el
@@ -51,17 +51,56 @@
   (message "Window is now %s" (if (window-dedicated-p) "dedicated" "undedicated")))
 
 (setq winner-dont-bind-my-keys t
-      winner-boring-buffers '("*helm*"
-                              "*helm buffers*"
-                              "*helm bookmarks*"
-                              "*helm make*"
-                              "*helm uni*"
-                              "*helm config*"
-                              "*Completions*"
+      winner-boring-buffers-rx '("\\*helm")
+      winner-boring-buffers '("*Completions*"
                               "*Help*"
                               "*compilation*"))
 
 (require-and-exec 'winner
   (winner-mode 1))
+
+;;; override winner-set with a winner-boring-buffers that accepts RE
+(defun winner-set (conf)
+  ;; For the format of `conf', see `winner-conf'.
+  (let* ((buffers nil)
+	 (alive
+          ;; Possibly update `winner-point-alist'
+	  (cl-loop for buf in (mapcar 'cdr (cdr conf))
+                   for pos = (winner-get-point buf nil)
+                   if (and pos (not (memq buf buffers)))
+                   do (push buf buffers)
+                   collect pos)))
+    (winner-set-conf (car conf))
+    (let (xwins)                        ; to be deleted
+
+      ;; Restore points
+      (dolist (win (winner-sorted-window-list))
+        (unless (and (pop alive)
+                     (setf (window-point win)
+                           (winner-get-point (window-buffer win) win))
+                     ;; modified start
+                     (not (let ((buffer-name (buffer-name (window-buffer win))))
+                          (or (find-if (lambda (re)
+                                        (string-match-p re buffer-name))
+                                      winner-boring-buffers-rx)
+                             (member buffer-name winner-boring-buffers)))))
+                     ;; modified end
+          (push win xwins)))            ; delete this window
+
+      ;; Restore marks
+      (save-current-buffer
+	(cl-loop for buf in buffers
+                 for entry = (cadr (assq buf winner-point-alist))
+                 do (progn (set-buffer buf)
+                           (set-mark (car entry))
+                           (setf (winner-active-region) (cdr entry)))))
+      ;; Delete windows, whose buffers are dead or boring.
+      ;; Return t if this is still a possible configuration.
+      (or (null xwins)
+	  (progn
+            (mapc 'delete-window (cdr xwins)) ; delete all but one
+            (unless (one-window-p t)
+              (delete-window (car xwins))
+              t))))))
 
 (provide 'cofi-windowing)

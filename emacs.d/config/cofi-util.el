295297f3f8fdbde1d9227decb186415d0dff0b1d
Michael Markert <markert.michael@googlemail.com>
2012-08-28 23:56:06 +0200

    [Emacs] util: Document `x-urgency-hint'.
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- emacs.d/config/cofi-util.el
+++ emacs.d/config/cofi-util.el
@@ -18,19 +18,21 @@
 (defun in-mode? (mode)
   (eq major-mode mode))
 
-(defun x-urgency-hint (frame arg &optional source)
+(defun x-urgency-hint (frame set &optional source)
+  "Set urgency hint for `frame' to `set' for `source'."
   (let* ((wm-hints (append (x-window-property
-			    "WM_HINTS" frame "WM_HINTS"
-			    (if source
-				source
-			      (string-to-number
-			       (frame-parameter frame 'outer-window-id)))
-			    nil t) nil))
-	 (flags (car wm-hints)))
+                            "WM_HINTS" frame "WM_HINTS"
+                            (if source
+                                source
+                              (string-to-number
+                               (frame-parameter frame 'outer-window-id)))
+                            nil t)
+                           nil))
+         (flags (car wm-hints)))
     (setcar wm-hints
-	    (if arg
-		(logior flags #x00000100)
-	      (logand flags #xFFFFFEFF)))
+            (if set
+                (logior flags #x00000100)
+              (logand flags #xFFFFFEFF)))
     (x-change-window-property "WM_HINTS" wm-hints frame "WM_HINTS" 32 t)))
 
 (defun add-to-hooks (fun hooks)

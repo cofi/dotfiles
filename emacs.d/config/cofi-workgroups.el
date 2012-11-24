ea4b6c7144dc11b284519b0be4117ac66bf60e00
Michael Markert <markert.michael@googlemail.com>
2012-11-24 13:06:36 +0100

    [Emacs] Explicitly enable workgroups-mode.
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- emacs.d/config/cofi-workgroups.el
+++ emacs.d/config/cofi-workgroups.el
@@ -1,12 +1,13 @@
 (add-to-loadpath "~/.elisp/vendor/workgroups/")
 
-(require-and-exec 'workgroups)
-
 (setq wg-mode-line-on t
       wg-mode-line-left-brace "{"
       wg-mode-line-right-brace "} "
       wg-query-for-save-on-emacs-exit nil
       wg-file (cofi/var-file "emacs/wg")
       wg-morph-on nil)
+
+(require-and-exec 'workgroups
+  (workgroups-mode))
 
 (provide 'cofi-workgroups)

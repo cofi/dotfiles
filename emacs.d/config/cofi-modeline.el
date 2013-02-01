2a5dd3a3430c7ee9c86bbe09347fadd43a220a71
Michael Markert <markert.michael@googlemail.com>
2013-02-01 18:37:50 +0100

    [Emacs] erc: Include tracked channels in non-erc modeline.
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- emacs.d/config/cofi-modeline.el
+++ emacs.d/config/cofi-modeline.el
@@ -62,6 +62,7 @@
                              help-echo "Minor mode\nmouse-1: Display minor mode menu\nmouse-2: Show help for minor mode, mouse-3: Toggle minor modes"
                              local-map ,mode-line-minor-mode-keymap)
                 ")%]"
+                (t erc-modified-channels-object)
                 " %-"
                 ))
 

24d0647ed4db9f85e18a9597d72d3456839b7a6b
Michael Markert <markert.michael@googlemail.com>
2013-02-11 18:21:03 +0100

    [Emacs] Add smartparens keys.
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- emacs.d/config/cofi-smartparens.el
+++ emacs.d/config/cofi-smartparens.el
@@ -39,4 +39,45 @@
 
 (add-to-hooks #'smartparens-mode '(text-mode-hook prog-mode-hook))
 
+(fill-keymap sp-keymap
+ "C-M-f" 'sp-forward-sexp
+ "C-M-b" 'sp-backward-sexp
+
+ "C-M-d" 'sp-down-sexp
+ "C-M-a" 'sp-backward-down-sexp
+
+ "C-M-e" 'sp-up-sexp
+ "C-M-u" 'sp-backward-up-sexp
+
+ "C-M-n" 'sp-next-sexp
+ "C-M-p" 'sp-previous-sexp
+
+ "C-M-k" 'sp-kill-sexp
+
+ "M-F" 'sp-forward-symbol
+ "M-B" 'sp-backward-symbol
+
+ "C-M-<backspace>" 'sp-backward-unwrap-sexp
+ "C-M-<delete>"    'sp-unwrap-sexp
+
+ "M-D"      'sp-splice-sexp
+ "M-<down>" 'sp-splice-sexp-killing-backward
+ "M-<up>"   'sp-splice-sexp-killing-forward)
+
+(defun cofi/grab-closing-paren ()
+  (interactive)
+  (unless (or (cofi/pos-in-comment-p (point))
+             (cofi/pos-in-string-p (point)))
+    (let ((old-pos (point))
+          (paren-pos (save-excursion (search-forward ")" nil t)
+                                     (1- (point)))))
+      (when (= (save-excursion
+                 (skip-chars-forward " \t\n")
+                 (point))
+               paren-pos)
+        (delete-region old-pos paren-pos)))))
+
+(evil-define-key 'insert lisp-mode-shared-map
+  (kbd "C-]") 'cofi/grab-closing-paren)
+
 (provide 'cofi-smartparens)

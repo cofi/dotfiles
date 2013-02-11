08d5721b141e90ffb1c554aa8b80b519b2eb1d13
Michael Markert <markert.michael@googlemail.com>
2013-02-11 18:16:21 +0100

    [Emacs] Change sp pairs.
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- emacs.d/config/cofi-smartparens.el
+++ emacs.d/config/cofi-smartparens.el
@@ -1,22 +1,36 @@
 (add-to-loadpath "~/.elisp/vendor/smartparens/")
+;;; replace default pairs
+(setq sp-pairs '((t
+                  .
+                  ((:open "\"" :close "\"" :actions (insert wrap))
+                   (:open "'"  :close "'"  :actions (insert wrap) :unless '(sp-point-after-word-p))
+                   (:open "("  :close ")"  :actions (insert wrap))
+                   (:open "["  :close "]"  :actions (insert wrap))
+                   (:open "{"  :close "}"  :actions (insert wrap))
+                   (:open "`"  :close "`"  :actions (insert wrap))))))
+
 (require 'smartparens)
 
 (push 'sldb-mode sp-ignore-modes-list)
-
-(sp-pair "'" nil :unless '(sp-point-after-word-p))
 
 (sp-with-modes '(emacs-lisp-mode
                  inferior-emacs-lisp-mode
                  lisp-interaction-mode
                  common-lisp-mode)
-  (sp-local-pair "'" nil :actions nil))
+  (sp-local-pair "'" nil :actions nil)
+  (sp-local-pair "`" "'" :when '(sp-in-string-p) :actions '(insert wrap)))
+
+(sp-with-modes '(text-mode)
+  (sp-local-pair "`" "'" :actions '(insert wrap)))
 
 (sp-with-modes '(tex-mode
                  plain-tex-mode
                  latex-mode)
   (sp-local-pair "$" "$")
   (sp-local-pair "\\[" "\\]")
-  (sp-local-pair "\\(" "\\)"))
+  (sp-local-pair "\\(" "\\)")
+  (sp-local-pair "\\left(" "\\right)")
+  (sp-local-pair "\\left\\{" "\\right\\}"))
 
 (sp-with-modes '(markdown-mode
                  rst-mode)

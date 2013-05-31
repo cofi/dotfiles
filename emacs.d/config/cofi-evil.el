680374d7119f7e60306df637797d5a6e829406c5
Michael Markert <markert.michael@googlemail.com>
2013-05-31 19:52:47 +0200

    [Emacs] Update evil.
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- emacs.d/config/cofi-evil.el
+++ emacs.d/config/cofi-evil.el
@@ -349,45 +349,6 @@ If `end' is nil `begin-or-fun' will be treated as a fun."
 (evil-define-key 'insert message-mode-map
   (kbd "RET") #'cofi/mail-return-keep-citation-markers)
 
-(defmacro cofi/enclose-ace-jump (&rest body)
-  `(let ((old-mark (mark))
-         (ace-jump-mode-scope 'window))
-     (remove-hook 'pre-command-hook #'evil-visual-pre-command t)
-     (remove-hook 'post-command-hook #'evil-visual-post-command t)
-     (unwind-protect
-         (progn
-           ,@body
-           (recursive-edit))
-       (if (evil-visual-state-p)
-           (progn
-             (add-hook 'pre-command-hook #'evil-visual-pre-command nil t)
-             (add-hook 'post-command-hook #'evil-visual-post-command nil t)
-             (set-mark old-mark))
-         (push-mark old-mark)))))
-
-(evil-define-motion evil-ace-jump-char-mode (count)
-  :type exclusive
-  (cofi/enclose-ace-jump
-   (ace-jump-mode 5)))
-
-(evil-define-motion evil-ace-jump-line-mode (count)
-  :type line
-  (cofi/enclose-ace-jump
-   (ace-jump-mode 9)))
-
-(evil-define-motion evil-ace-jump-word-mode (count)
-  :type exclusive
-  (cofi/enclose-ace-jump
-   (ace-jump-mode 1)))
-
-(evil-define-motion evil-ace-jump-char-to-mode (count)
-  :type exclusive
-  (cofi/enclose-ace-jump
-   (ace-jump-mode 5)
-   (forward-char -1)))
-
-(add-hook 'ace-jump-mode-end-hook 'exit-recursive-edit)
-
 (evil-add-hjkl-bindings *bc-menu-mode-map* 'emacs)
 
 (evil-add-hjkl-bindings magit-branch-manager-mode-map 'emacs

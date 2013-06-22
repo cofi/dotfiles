77833362c9a6cc755b342a4c1d4c2f0520c5ef74
Michael Markert <markert.michael@googlemail.com>
2013-06-22 23:06:21 +0200

    [Emacs] Switch to ggtags.
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- emacs.d/config/cofi-tags.el
+++ emacs.d/config/cofi-tags.el
@@ -1,25 +1,9 @@
-(eval-after-load "gtags"
-  '(fill-keymap gtags-mode-map
-                "M-,"   #'gtags-find-tag-from-here
-                "C-M-," #'gtags-find-rtag))
+(add-hook 'c-mode-common-hook #'ggtags-mode)
 
-(defun cofi/update-or-create-gtags (&optional create)
-  "Update a global database or create a new one if none exists
-and `create' is passed (or command is interactively called with
-prefix."
-  (interactive "P")
-  (if (= 0 (call-process "global" nil nil nil "-p"))
-      (start-process "update global" nil "global" "-u")
-    (when create
-      (let ((default-directory (read-directory-name "Source directory: ")))
-        (start-process "create global" nil "gtags")))))
-
-(defun cofi/maybe-enable-gtags-mode ()
-  (when (= 0 (call-process "global" nil nil nil "-p"))
-    (gtags-mode)))
-
-(add-hook 'c-mode-common-hookmmon-hook #'gtags-mode)
-(add-hook 'c-mode-common-hook #'cofi/maybe-enable-gtags-mode)
-(add-hook 'c-mode-common-hook #'cofi/update-or-create-gtags)
+(eval-after-load "ggtags"
+  '(fill-keymap ggtags-mode-map
+     "C-c ." 'ggtags-find-tag
+     "C-c ," 'ggtags-find-tag-resume
+     "C-c /" 'ggtags-navigation-mode-done))
 
 (provide 'cofi-tags)

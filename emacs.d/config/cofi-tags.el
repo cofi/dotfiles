1daaed5a74e41574a1fe1d05633aac3521253e87
Michael Markert <markert.michael@googlemail.com>
2014-05-02 15:59:38 +0200

    [Emacs] Use dwim ggtags.

--- emacs.d/config/cofi-tags.el
+++ emacs.d/config/cofi-tags.el
@@ -2,7 +2,7 @@
 
 (eval-after-load "ggtags"
   '(fill-keymap ggtags-mode-map
-     "C-c ." 'ggtags-find-tag
+     "C-c ." 'ggtags-find-tag-dwim
      "C-c ," 'ggtags-find-tag-resume
      "C-c /" 'ggtags-navigation-mode-done))
 
